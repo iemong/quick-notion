06d6d33b71693bc5d4022b58d398c2607399c50c
iemong <kiyoshidainagon@gmail.com>
2022-07-22 22:13:18 +0900

    add a rich text editor

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' as Quill;
 import 'package:quick_notion/second_page.dart';
 
 class FirstPage extends StatelessWidget {
   FirstPage({Key? key}) : super(key: key);
 
   String nameText = '';
+  Quill.QuillController _controller = Quill.QuillController.basic();
 
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: Column(
           children: [
+            Quill.QuillToolbar.basic(controller: _controller),
+            Expanded(
+              child: Container(
+                child: Quill.QuillEditor.basic(controller: _controller, readOnly: false),
+              ),
+            ),
             Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
             TextField(
               onChanged: (text) {
@@ -24,7 +33,7 @@ class FirstPage extends StatelessWidget {
                 Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage(name: nameText))
                 );
               },
-              child: const Text('次の画面へ'),
+              child: Text('次の画面へ'),
             ),
           ],
         ),
